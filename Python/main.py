ce62cbe9ff1145e8e049db54dc6fff7df1c7b4e6
gege-rald <geraldjohnmatutinasotto@gmail.com>
2024-05-31 18:01:40 +0800

    changes

--- Python/main.py
+++ Python/main.py
@@ -15,7 +15,7 @@ def base():
 def open_street_map():
     map = folium.Map(
         location=[13.044031000000002, 123.45465545767141],
-        tiles='Stamen Toner',
+        tiles='Stamen Toner ',
         zoom_start=13,
         attr='Map tiles by <a href="https://stamen.com">Stamen Design</a>, under <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, under <a href="https://www.openstreetmap.org/copyright">ODbL</a>.'
     )
